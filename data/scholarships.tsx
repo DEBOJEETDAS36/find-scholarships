21a42e3afb532c0e302f31d198ecf11798d229c9
Debojeet Das <debojeetdas218@gmail.com>
2026-01-14 01:18:05 +0530

    Added dummy scholarship data

---
+++ data/scholarships.tsx
@@ -0,0 +1,20 @@
+export const scholarships = [
+  {
+    id: "1",
+    name: "National Merit Scholarship",
+    category: "SC",
+    state: "India",
+    education: "UG",
+    deadline: "2026-03-31",
+    link: "https://example.com"
+  },
+  {
+    id: "2",
+    name: "Minority Girls Scholarship",
+    category: "Minority",
+    state: "West Bengal",
+    education: "PG",
+    deadline: "2026-02-15",
+    link: "https://example.com"
+  }
+];
