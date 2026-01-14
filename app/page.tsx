4b311f086d17e07a03369bdce0fb4f72c6714586
Debojeet Das <debojeetdas218@gmail.com>
2026-01-15 02:43:11 +0530

    Search field added & working

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,54 @@
+"use client";
+
+import { useState } from "react";
 import { scholarships } from "../data/scholarships";
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredScholarships = scholarships.filter((s) =>
+    s.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
         Scholarship Finder
       </h1>
 
+      {/* Search Bar */}
+      <div className="max-w-3xl mx-auto mb-6 text-blue-300">
+        <input
+          type="text"
+          placeholder="Search scholarships..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+      {/* Scholarship List */}
       <div className="max-w-3xl mx-auto space-y-4">
-        {scholarships.map((s) => (
-          <div
-            key={s.id}
-            className="bg-white p-4 rounded-lg shadow"
-          >
-            <h2 className="text-xl font-semibold">{s.name}</h2>
-            <p className="text-sm text-gray-600">
-              Category: {s.category} | State: {s.state}
-            </p>
-            <p className="text-sm text-gray-600">
-              Deadline: {s.deadline}
-            </p>
-          </div>
-        ))}
+        {filteredScholarships.length > 0 ? (
+          filteredScholarships.map((s) => (
+            <div
+              key={s.id}
+              className="bg-white p-4 rounded-lg shadow"
+            >
+              <h2 className="text-xl text-black font-semibold">{s.name}</h2>
+              <p className="text-sm text-gray-600">
+                Category: {s.category} | State: {s.state}
+              </p>
+              <p className="text-sm text-gray-600">
+                Deadline: {s.deadline}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p className="text-center text-gray-500">
+            No scholarships found
+          </p>
+        )}
       </div>
     </main>
   );
